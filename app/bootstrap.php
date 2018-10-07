8197314549f09997c555d6532e533d1db419bc0c
AmrAlaaYassen <amralaa566@gmail.com>
2018-10-08 00:05:22 +0200

    add auto-loader for libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,12 @@
 <?php 
-    // load our libraries 
-    require_once 'libraries/Controller.php';
-    require_once 'libraries/Core.php';
-    require_once 'libraries/Database.php';
+
+    //  load config file 
+    require_once 'config/config.php';
+  
+    
+
+    // auto load core libraries 
+    spl_autoload_register(function($className){
+        require_once 'libraries/'.$className.'.php' ;
+    });
 ?>
---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php 
+    // APP root
+    define('APPROOT',dirname(dirname(__FILE__))) ; 
+
+    // URL Root 
+
+    define('URLROOT','http://localhost/adamframework');
+
+    // Site Name 
+    define('SITENAME','adamframework');
+?> 
